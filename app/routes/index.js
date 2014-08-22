9ea59d370317d8661f29faab9349c4b1ae2af954
dagda1 <dagda1@scotalt.net>
2014-08-22 09:58:16 +0100

    scratch

--- app/routes/index.js
+++ app/routes/index.js
@@ -1,25 +1,4 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
-  model: function(){
-    var speaker  = Ember.Object.createWithMixins({
-      firstName: 'Paul',
-      surname: 'Cowan',
-      age: 42,
-      mood: 'calm',
-      isCalm: Ember.computed.equal('mood', 'calm'),
-
-      isAngry: Ember.computed.equal('mood', 'angry'),
-
-      fullName: Ember.computed('firstName', 'surname', function(){
-        return this.get('firstName') + " " + this.get('surname');
-      }),
-
-      isOld: Ember.computed('age', function() {
-        return this.get('age') >= 42;
-      })
-    });
-
-    return speaker;
-  }
 });
