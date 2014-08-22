6613c20810f393be276943e9d065dc90bad77bd4
dagda1 <dagda1@scotalt.net>
2014-08-22 09:54:00 +0100

    first commit

--- app/app.js
+++ app/app.js
@@ -5,8 +5,26 @@ import loadInitializers from 'ember/load-initializers';
 Ember.MODEL_FACTORY_INJECTIONS = true;
 
 var App = Ember.Application.extend({
-  modulePrefix: 'talk-ember', // TODO: loaded via config
+  modulePrefix: 'talk-ember',
   Resolver: Resolver
+});
+
+App.speaker = Ember.Object.createWithMixins({
+  firstName: 'Paul',
+  surname: 'Cowan',
+  age: 42,
+  mood: 'calm',
+  isCalm: Ember.computed.equal('mood', 'calm'),
+
+  isAngry: Ember.computed.equal('mood', 'angry'),
+
+  fullName: Ember.computed('firstName', 'surname', function(){
+    return this.get('firstName') + " " + this.get('surname');
+  }),
+
+  isOld: Ember.computed('age', function() {
+    return this.get('age') >= 42;
+  })
 });
 
 loadInitializers(App, 'talk-ember');
