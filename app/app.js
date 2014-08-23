dedffecb98865e8423a1fec1791c1a97d1f729a0
dagda1 <dagda1@scotalt.net>
2014-08-23 10:56:08 +0100

    remove

--- app/app.js
+++ app/app.js
@@ -9,24 +9,6 @@ var App = Ember.Application.extend({
   Resolver: Resolver
 });
 
-App.speaker = Ember.Object.createWithMixins({
-  firstName: 'Paul',
-  surname: 'Cowan',
-  age: 42,
-  mood: 'calm',
-  isCalm: Ember.computed.equal('mood', 'calm'),
-
-  isAngry: Ember.computed.equal('mood', 'angry'),
-
-  fullName: Ember.computed('firstName', 'surname', function(){
-    return this.get('firstName') + " " + this.get('surname');
-  }),
-
-  isOld: Ember.computed('age', function() {
-    return this.get('age') >= 42;
-  })
-});
-
 loadInitializers(App, 'talk-ember');
 
 export default App;
